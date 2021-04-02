b08bbffb81851e48056e440d8f958ea1930d13f1
Sudipto Ghosh <sudipto@ghosh.pro>
2021-04-02 17:35:04 +0530

    initial commit

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '52ffbd5c-3470-406a-98e0-61c86da40728'
+};
---
+++ frontend/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="uk-section">
+      <div class="uk-container uk-container-large">
+        <h1>My Blog</h1>
+        <Articles :articles="articles"></Articles>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import articlesQuery from '~/apollo/queries/article/articles'
+import Articles from '~/components/Article'
+
+export default {
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  components: {
+    Articles,
+  },
+  apollo: {
+    articles: {
+      prefetch: true,
+      query: articlesQuery,
+      variables() {
+        return { id: parseInt(this.$route.params.id) }
+      },
+    },
+  },
+}
+</script>
